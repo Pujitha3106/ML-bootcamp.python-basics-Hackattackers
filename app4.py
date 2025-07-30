495e097655c0c1f40e2aec6c49df55073577b1ac
Pujitha3106 <pujithachowdary696@gmail.com>
2025-07-30 13:48:47 +0530

    adding all files

---
+++ app4.py
@@ -0,0 +1,6 @@
+number=10
+scores=[10,20,30]
+names=["Alexa","Siri","java"]
+for i in names: 
+    print(i)
+    
---
+++ app5.py
@@ -0,0 +1,4 @@
+def  greeting(name):
+    print("Hello world"+name)
+
+greeting("puji")
